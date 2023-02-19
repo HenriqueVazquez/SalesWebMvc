493dab9df7db7dd92a80d8e00856fab59218f3fd
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-02-19 17:21:25 -0300

    Other entities and second migration

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,7 +1,29 @@
 ﻿namespace SalesWebMvc.Models {
     public class Department {
         public int Id { get; set; }
-
         public string Name { get; set; }
+
+        public ICollection<Seller> Seller { get; set; } = new List<Seller>();
+
+
+        public Department()
+        {
+
+        }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public  void AddSeller(Seller seller)
+        {
+            Seller.Add(seller);
+        }
+
+        public double TotalSalse(DateTime initial, DateTime final)
+        {
+            return Seller.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
