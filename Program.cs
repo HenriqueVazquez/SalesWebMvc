7ee089a85412375e05ab055f752bb4d8e0034de3
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-02-20 10:50:32 -0300

    SellerFormViewModel and Department select component

--- Program.cs
+++ Program.cs
@@ -22,7 +22,8 @@ internal class Program
         builder.Services.AddControllersWithViews();
         builder.Services.AddScoped<SeedingService>();
         builder.Services.AddScoped<SellerService>();
-
+        builder.Services.AddScoped<DepartmentService>();
+        
         var app = builder.Build();      
 
 
